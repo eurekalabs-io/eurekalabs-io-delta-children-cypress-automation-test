784e74936456e80722f110966c8bbb3b23da94ba
fbosio <fbosio@eurekalabs.io>
2024-10-18 10:46:20 -0300

    Change to repository of Eureka

--- Cypress/CheckoutPage.js
+++
@@ -1,3 +0,0 @@
-import BasePage from '../BasePage';
-
-export default class CheckoutPage extends BasePage { }
--- Cypress/page-objects/pages/ProductDetailsPage.js
+++ Cypress/page-objects/pages/ProductDetailsPage.js
@@ -2,15 +2,18 @@ import BasePage from '../BasePage';
 
 export default class ProductDetailsPage extends BasePage {
   static addToCart = '.cb-bundle-layout__left > .cb-cart > .cb-tooltip-wrapper > #bundle-add-to-cart'
-  static addVariant = '.components-section > :nth-child(1) > .cb-customizer-wrapper > .cb-customizer > .cb-customizer-footer > .v2-button--primary'
-  static countVariant = 'div.cb-bundle-layout__left > div.section-slider > div > div:nth-child(1) >'
-  static countVariantKids = 'div.cb-bundle-layout__left > div.section-slider > div > div:nth-child(1) > div > div.cb-product-list-item-header.cb-product-list-item-header--customized > div'
+  static addProducts = '.components-section > :nth-child(1) > .cb-customizer-wrapper > .cb-customizer > .cb-customizer-footer > .v2-button--primary'
+  
+  //Product list that are not default of bundle for Nursery an Kids Sets 
+  static countProducts = 'div.cb-bundle-layout__left > div.section-slider > div > div:nth-child(1) >'
+  static countProductsKids = 'div.cb-bundle-layout__left > div.section-slider > div > div:nth-child(1) > div > div.cb-product-list-item-header.cb-product-list-item-header--customized > div'
   
 
   
-  static selectVariants() {
+//Count products that are not default and select
+  static selectProducts() {
     let countOfElements = 0;
-    cy.get(this.countVariant).then($elements => {
+    cy.get(this.countProducts).then($elements => {
       countOfElements = $elements.length;
       for (let cuenta = 1; cuenta <= countOfElements; cuenta++) {
         cy.get(`.components-section > :nth-child(1) > :nth-child(${cuenta}) > .cb-product-list-item-content`).click({force:true})
@@ -20,13 +23,14 @@ export default class ProductDetailsPage extends BasePage {
     });
   }
 
-  static selectVariantsKidsSets() {
+//Count products that are not default and select
+  static selectProductsKidsSets() {
     let countOfElements = 0;
-    cy.get(this.countVariantKids).then($elements => {
+    cy.get(this.countProductsKids).then($elements => {
       countOfElements = $elements.length;
       for (let cuenta = 1; cuenta <= countOfElements; cuenta++) {
         cy.get(`.components-section > :nth-child(1) > :nth-child(${cuenta}) > .cb-product-list-item-content`).click({force:true})
-        cy.get(this.addVariant).click()
+        cy.get(this.addProducts).click()
         BasePage.pause(1000)
       }
     });
